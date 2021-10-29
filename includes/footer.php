11b848ef50a797171f9ad069eba7fee64e7d36fc
FelipeHoffmannOPO <85354534+FelipeHoffmannOPO@users.noreply.github.com>
2021-10-29 12:20:46 -0400

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<footer class="page-footer laranja">
+      <div class="container">
+        <div class="row">
+          <div class="col l12 s12">
+            <h5 id="logo-container" class="white-text">
+              <img src="assets/imagens/Estrela_Motors-removebg-preview.png" />
+            </h5>
+            <p class="grey-text text-lighten-4">
+              Copyright 2021 - Todos os Direitos Reservados.
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    </footer>
+    <script>
+       M.AutoInit();
+    </script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+//header
+include_once 'includes/header.php';
+?>
+
+<div class="section no-pad-bot" id="index-banner">
+      <div class="container">
+        <br /><br />
+        <div class="row center">
+          <div class="col s12 m6 16 x16">
+            <h1 class="header center">
+              Gerenciamento de Estoque de Carros
+            </h1>
+
+            <a
+              href=""
+              id="download-button"
+              class="btn-large waves-effect waves-light laranja"
+              >Entrar</a
+            >
+          </div>
+          <div class="col s12 m6 16 x16">
+            <a id="imagem-conteiner" href="#"
+              ><img src="assets/imagens/Estrela_Motors-removebg-preview.png" /></a>
+          </div>
+        </div>
+        <br /><br />
+      </div>
+    </div>
+
+<?php
+//footer
+include_once 'includes/footer.php';
+?>
